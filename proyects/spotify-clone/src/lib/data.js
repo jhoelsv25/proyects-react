c8a5f65fe2eeffde8c7156076b4ca2dde353d162
jhoel silvestre <jhoel.sv25@gmail.com>
2023-10-19 21:03:43 -0500

    add icons

--- proyects/spotify-clone/src/lib/data.js
+++ proyects/spotify-clone/src/lib/data.js
@@ -1,14 +1,5 @@
 import { colors } from "./colors";
 
-/* export interface Playlist {
-  id: string;
-  albumId: number;
-  title: string;
-  color: (typeof colors)[keyof typeof colors];
-  cover: string;
-  artists: string[];
-} */
-
 export const playlists = [
   {
     id: "1",
@@ -77,16 +68,6 @@ export const allPlaylists = [
   ...morePlaylists,
   ...sidebarPlaylists,
 ];
-
-/* export interface Song {
-  id: number;
-  albumId: number;
-  title: string;
-  image: string;
-  artists: string[];
-  album: string;
-  duration: string;
-} */
 
 export const songs = [
   {
